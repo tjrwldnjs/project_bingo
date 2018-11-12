8b0c59458aad93773bd09760c7a87c4055348cea
tjrwldnjs <sue06024@naver.com>
2018-11-12 17:25:30 +0900

    bingo

---
+++ bingo function.c
@@ -0,0 +1,21 @@
+int N;
+
+void initiate_bingo(int bingo[N][N])
+	{
+		int NULL;
+				
+		srand((unsigned)time(NULL));
+		
+		int i; 
+		int j;
+		
+		for (i=0; i<N; i++)
+		{
+			for (j=0; j<N; j++)
+			{
+				printf("%d", bingo[N][N]);
+			}
+		} printf("\n");
+		
+		return 0;
+	};
---
+++ bingo.c
@@ -0,0 +1,13 @@
+
+
+int main(int argc, char *argv[]) {
+
+	printf("Start Bingo Game!");
+	
+	int N;
+	
+	initiate_bingo(int bingo[N][N]);
+
+	 
+	return 0;
+}
